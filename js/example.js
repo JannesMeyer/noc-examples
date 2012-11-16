057e261d0c09017f384b680898458012e2092df9
Jannes <jannes.meyer@gmail.com>
2012-11-16 00:12:02 -0500

    Finish example 1.7: Motion 101 (velocity)
    Having some fun with multiple balls

--- js/example.js
+++ js/example.js
@@ -1,27 +1,121 @@
-// PVector constructor
+/**
+ * Returns a random natural number between the lower and upper bound (including both bounds).
+ * Can alternatively be called with just one parameter, in which case the lower
+ * bound will be assumed to be zero.
+ *
+ * @author Jannes Meyer
+ */
+function random(lower, upper) {
+	if (typeof upper === 'undefined') {
+		// Use the first parameter as upper bound and 0 as lower bound
+		return Math.floor(Math.random() * (lower + 1));
+	} else {
+		// We are always rounding down, so we have to extend the upper bound by one
+		return Math.floor(Math.random() * (upper + 1 - lower)) + lower;
+	}
+}
+/**
+ * Returns a random real number between the lower and upper bound
+ * (including the lower bound, but not the upper bound).
+ * Can alternatively be called with just one parameter, in which case the lower
+ * bound will be assumed to be zero.
+ *
+ * @author Jannes Meyer
+ */
+function randomFloat(lower, upper) {
+	if (typeof upper === 'undefined') {
+		// Use the first parameter as upper bound and 0 as lower bound
+		return Math.random() * lower;
+	} else {
+		return Math.random() * (upper - lower) + lower;
+	}
+}
+
+
+// PVector object constructor
 PVector = function(x, y) {
 	this.x = x;
 	this.y = y;
 };
-// Add another vector
+// Addition
 PVector.prototype.add = function(v) {
-	// TODO: check if the v is a PVector
-	if (typeof v === 'undefined') {
-		console.error('Missing paramter for PVector.add');
-	}
-
-	// Addition
 	this.x += v.x;
 	this.y += v.y;
 };
+// Subtraction
+PVector.prototype.sub = function(v) {
+	this.x -= v.x;
+	this.y -= v.y;
+};
+// Multiplication
+PVector.prototype.mult = function(factor) {
+	this.x *= factor;
+	this.y *= factor;
+};
+// Division
+PVector.prototype.div = function(dividend) {
+	this.x /= dividend;
+	this.y /= dividend;
+};
+// Calculate the magnitude
+PVector.prototype.mag = function() {
+	return Math.sqrt(this.x*this.x + this.y*this.y);
+};
+// Normalization
+PVector.prototype.normalize = function() {
+	// Inlined function calls for better performance
+	var magnitude = Math.sqrt(this.x*this.x + this.y*this.y);
+	if (magnitude !== 0) {
+		this.x /= magnitude;
+		this.y /= magnitude;
+	}
+};
+// Check for null vector
+PVector.prototype.isNull = function() {
+	return this.x === 0 && this.y === 0;
+};
+
+
+
+// Mover object constructor
+function Mover() {
+	this.location = new PVector(random(stage.canvas.width), random(stage.canvas.height));
+	this.velocity = new PVector(randomFloat(-2, 2), randomFloat(-2, 2));
+	this.radius = 16;
+
+	// Create ball
+	this.shape = new createjs.Shape();
+	this.shape.graphics
+		.beginFill('#000')
+		.drawCircle(0, 0, this.radius);
+	stage.addChild(this.shape);
+}
+Mover.prototype.update = function() {
+	// The Mover moves
+	this.location.add(this.velocity);
+};
+Mover.prototype.display = function() {
+	this.shape.x = this.location.x;
+	this.shape.y = this.location.y;
+};
+Mover.prototype.checkEdges = function() {
+	if (this.location.x > stage.canvas.width) {
+		this.location.x = 0;
+	} else if (this.location.x < 0) {
+		this.location.x = stage.canvas.width;
+	}
+
+	if (this.location.y > stage.canvas.height) {
+		this.location.y = 0;
+	} else if (this.location.y < 0) {
+		this.location.y = stage.canvas.height;
+	}
+};
+
+
 
 var stage;
-var ball = {
-	location: new PVector(100, 100),
-	velocity: new PVector(4, 7),
-	radius: 20
-};
-var ballShape;
+var movers = [];
 
 window.addEventListener('DOMContentLoaded', init, false);
 
@@ -37,10 +131,16 @@ function init() {
 	resizeToFullscreen();
 	window.addEventListener('resize', resizeToFullscreen, false);
 
-	// Create ball
-	ballShape = new createjs.Shape();
-	ballShape.graphics.beginFill('#f00').drawCircle(0, 0, ball.radius);
-	stage.addChild(ballShape);
+	// Create Mover
+	movers.push(new Mover());
+	movers.push(new Mover());
+	movers.push(new Mover());
+	movers.push(new Mover());
+	movers.push(new Mover());
+	movers.push(new Mover());
+	movers.push(new Mover());
+	movers.push(new Mover());
+	movers.push(new Mover());
 	
 	// Start ticker
 	createjs.Ticker.addListener(window);
@@ -48,20 +148,11 @@ function init() {
 }
 
 function tick() {
-	// Move the ball
-	ball.location.add(ball.velocity);
-
-	// Check for bouncing
-	if (((ball.location.x + ball.radius) > stage.canvas.width) || (ball.location.x < ball.radius)) {
-		ball.velocity.x *= -1;
+	for (var i = 0, len = movers.length; i < len; ++i) {
+		movers[i].update();
+		movers[i].checkEdges();
+		movers[i].display();
 	}
-	if (((ball.location.y + ball.radius) > stage.canvas.height) || (ball.location.y < ball.radius)) {
-		ball.velocity.y *= -1;
-	}
-
-	// Update position
-	ballShape.x = ball.location.x;
-	ballShape.y = ball.location.y;
 
 	stage.update();
 }
