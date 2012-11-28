41dc28ba2ecc8e74962213274f66cf4765a12f6d
Jannes <jannes.meyer@gmail.com>
2012-11-27 23:12:59 -0500

    Exercise I.06 with Processing.js

---
+++ exercise-I.06/main.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', function() {
+	new Processing('canvas', sketch);
+}, false);
+
+function sketch(p) {
+
+	p.setup = function() {
+		p.size(640, 480);
+		p.background(0);
+
+		// disable clearing and redrawing
+		p.noLoop();
+	};
+
+	// parameters
+	var increment = 0.02;
+
+	// draw function
+	p.draw = function() {
+		p.loadPixels();
+		var xoff = 0;
+
+		for(var x = 0; x < p.width; x++) {
+ 			xoff += increment;
+
+ 			var yoff = 0.0;
+ 			for(var y = 0; y < p.height; y++) {
+ 				yoff += increment;
+
+ 				var bright = p.noise(xoff, yoff) * 255;
+ 				p.pixels.setPixel(x+y*p.width, p.color(bright));
+ 			}
+ 		}
+
+		p.updatePixels();
+	};
+}
